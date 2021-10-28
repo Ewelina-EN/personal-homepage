b75942a223e3189ef164516d16f1b533a3be360e
Ewelina Niewęgłowska <nieweglowska.ewelina@gmail.com>
2021-10-28 16:24:00 +0200

    add saga, change store

--- src/core/store.js
+++ src/core/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import personalHomepageReducer from "../features/PersonalHomepage/personalHomepageSlice";
+import createSagaMiddleware from "@redux-saga/core";
+import saga from "./saga";
+import personalHomepageReducer from "../features/personalHomapage/personalHomepageSlice";
 
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         personalHomepage: personalHomepageReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(saga);
 
 export default store;
 
