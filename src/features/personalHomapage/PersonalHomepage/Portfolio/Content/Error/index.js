0c7f2f21bc5a14384b00b048b114b0e3a60c763b
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-11-12 15:01:50 +0100

    non-breaking space added

--- src/features/personalHomapage/PersonalHomepage/Portfolio/Content/Error/index.js
+++ src/features/personalHomapage/PersonalHomepage/Portfolio/Content/Error/index.js
@@ -5,16 +5,13 @@ import { ReactComponent as ErrorIcon } from "./error.svg";
 export const Error = () => (
     <Wrapper>
         <ErrorIcon />
-        <Header>Ooops! Something went wrong...</Header>
+        <Header>Ooops! Something went&nbsp;wrong...</Header>
         <Paragraph>
-            Sorry, failed to load Github projects.
-            You can check  them directly on Github.
+            Sorry, failed to load Github&nbsp;projects.
+            You can check  them directly&nbsp;on&nbsp;Github.
         </Paragraph>
-        <ButtonLink
-            href={`https://github.com/Ewelina-EN`} target="_blank"
-        >
+        <ButtonLink href={`https://github.com/Ewelina-EN`} target="_blank">
             Go to Github
         </ButtonLink>
-
     </Wrapper>
 );
