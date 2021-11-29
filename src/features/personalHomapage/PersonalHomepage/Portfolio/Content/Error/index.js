94409c4fec53c9c33d272e071b9c28c506f43340
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-11-29 14:47:19 +0100

    change in error component

--- src/features/personalHomapage/PersonalHomepage/Portfolio/Content/Error/index.js
+++ src/features/personalHomapage/PersonalHomepage/Portfolio/Content/Error/index.js
@@ -1,16 +1,17 @@
 import { ButtonLink } from "../../../AboutMe/styled";
 import { Wrapper, Header, Paragraph } from "./styled";
 import { ReactComponent as ErrorIcon } from "./error.svg";
+import { userName } from "../../userName";
 
 export const Error = () => (
     <Wrapper>
         <ErrorIcon />
         <Header>Ooops! Something went&nbsp;wrong...</Header>
         <Paragraph>
-            Sorry, failed to load Github&nbsp;projects.
+            Sorry, failed to load Github&nbsp;projects.<br />
             You can check  them directly&nbsp;on&nbsp;Github.
         </Paragraph>
-        <ButtonLink href={`https://github.com/Ewelina-EN`} target="_blank">
+        <ButtonLink href={`https://github.com/${userName}`} target="_blank">
             Go to Github
         </ButtonLink>
     </Wrapper>
